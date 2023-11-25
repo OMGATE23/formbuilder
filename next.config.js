e0af6eed1facdaa5fb185b0891dce176c62cfd4a
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-11-25 20:30:35 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    async headers() {
+        return [{
+            source: "/(.*)",
+            headers: [{
+                key: "Cross-Origin-Opener-Policy",
+                value: "same-origin-allow-popups",
+            }, ],
+        }, ];
+    },
+}
+module.exports = nextConfig
