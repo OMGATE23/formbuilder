be769b8a71202c78e8d40000e3832faab2b826f9
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-12-20 11:22:27 +0530

    feat : home page login

--- app/home/page.js
+++ app/home/page.js
@@ -1,28 +1,36 @@
 'use client'
 import UserHeader from '@/components/header/UserHeader'
 import { useAuthContext } from '@/hooks/useAuthContext'
+import useLogin from '@/hooks/useLogin'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 const Home = () => {
     const {user} = useAuthContext()
+    const {login} = useLogin()
+    console.log(user)
     useEffect(() => {
-        if(!user){
-        redirect('/')
-        }
-    } , [user])
+        (async() => {
+            if(!user){
+                await login()
+            }
+        })()
+    } , [])
+
   return (
     <div className='h-[100vh]'>
-        <UserHeader/>
-        <div className='w-full h-[80%] flex flex-col md:flex-row gap-8 justify-center items-center'>
-            <Link className='home-links shadow-md bg-teal-100 hover:bg-teal-100 md:bg-white' href='/create'>
-                Create Form
-            </Link>
-            <Link className='home-links shadow-md bg-sky-100 hover:bg-sky-100 md:bg-white' href='/submissions'>
-                See submissions
-            </Link>
-        </div>
+        {user && <>
+            <UserHeader/>
+            <div className='w-full h-[80%] flex flex-col md:flex-row gap-8 justify-center items-center text-center'>
+                <Link className='home-links shadow-md bg-teal-100 hover:bg-teal-100 md:bg-white' href='/create'>
+                    Create Form
+                </Link>
+                <Link className='home-links shadow-md bg-sky-100 hover:bg-sky-100 md:bg-white' href='/submissions'>
+                    See submissions
+                </Link>
+            </div>
+        </>}
     </div>
   )
 }
